1cf0cf36ffda4b43a76a2028a558127d44ab53cc
DazHaze <dhayes1.wex@gmail.com>
2022-06-29 15:02:19 +0000

    add sort selector functionality and back to top button

--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from .models import Product, Category
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Lower
 # Create your views here.
 
 def all_products(request):
