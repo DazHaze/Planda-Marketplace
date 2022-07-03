5916ac3c299e3931d855acae75e07e7cb720d4b2
DazHaze <dhayes1.wex@gmail.com>
2022-07-03 22:58:22 +0000

    Removed secret key

--- planda_webshop/settings.py
+++ planda_webshop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z!^!^vooa=6076=_64jqg44i@tea_3k5g+=7#dk%$68%)4f*n('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['planda-webshop.herokuapp.com', 'localhost']
 
